113f71009d3649e595882133211c3878697672de
Javier <javieerperez@hotmail.com>
2017-01-30 16:57:30 +0100

    Primera versión

---
+++ src/Principal/Persona.java
@@ -0,0 +1,32 @@
+package Principal;
+
+public class Persona {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+	public byte setEdad;
+
+	public void setPeso(int i) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	public void setNombre(String string) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	public void setEdad(byte b) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	public int getEdad() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+}
